3a35594664bd4c4d23f66f0aa521cd2f6376b5c4
willianmolina <ibanez.molina@hotmail.com>
2019-06-05 23:45:07 -0300

    Update

--- src/bitcoin/Cliente.java
+++ src/bitcoin/Cliente.java
@@ -6,53 +6,80 @@ import java.util.logging.Logger;
 public class Cliente extends Thread {
 
     public String nome;
-    public Double dinheiro;
     public Integer nbitcoins;
+    public Double dinheiro;
     public Corretora corretora;
-    public Random gerador = new Random();
     public int quantidade;
+      
+    public Random gerador = new Random();
+  
 
     public Cliente(String nome, Corretora corretora) {
         this.nome = nome;
-        this.corretora = corretora;
-        this.dinheiro = 1000000.00;
         this.nbitcoins = 0;
+        this.dinheiro = 1000000.00;
+        this.corretora = corretora;
+       
+       
+    }
+    
+    public void comprar(int quantidade) {
+        nbitcoins = nbitcoins + quantidade ;
+        dinheiro = dinheiro - corretora.venderBitcoin(quantidade);
     }
 
+    public void vender(int quantidade) {
+        nbitcoins = nbitcoins - quantidade;
+        dinheiro = dinheiro + corretora.comprarBitcoin(quantidade);
+    }
+    
+     public void negociacao() {
+        quantidade = gerador.nextInt(4) + 1;
+        
+        if (nbitcoins == 0 && dinheiro >= corretora.valorBTC * quantidade && corretora.numBitcoins>=quantidade) {
+            this.comprar(quantidade);
+            
+        } 
+        
+        else if (gerador.nextInt(3) == 1 && dinheiro >= (corretora.valorBTC * quantidade) && corretora.numBitcoins>=quantidade) {
+            this.comprar(quantidade);
+            
+        } 
+        
+        else if (nbitcoins>=quantidade && corretora.dinheiro >= corretora.valorBTC * quantidade) {
+            this.vender(quantidade);
+        }
+
+    }
+
+    
     @Override
     public void run() {
         while (true) {
-            negociar();
-            System.out.println("Cliente: " + nome + " - Quantidade de Bitcoins: " + nbitcoins + " - Dinheiro: " + dinheiro);
+            negociacao();
+            if(nbitcoins == 0){
+                System.out.println("Cliente: " + nome + " - Quantidade de Bitcoins: " + nbitcoins + " - Dinheiro: " + dinheiro + " - [Cliente propício à compra]");
+            }
+            else if(nbitcoins > 0 && nbitcoins < 4){
+                System.out.println("Cliente: " + nome + " - Quantidade de Bitcoins: " + nbitcoins + " - Dinheiro: " + dinheiro + " - [Cliente propício à compra ou venda]");
+            }
+            else{
+            System.out.println("Cliente: " + nome + " - Quantidade de Bitcoins: " + nbitcoins + " - Dinheiro: " + dinheiro + " - [Cliente propício à venda]");
+        }
+            
             try {
                 Thread.sleep(5000);
-            } catch (InterruptedException ex) {
+            } 
+            
+            catch (InterruptedException ex) {
                 Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+                
             }
         }
 
     }
 
-    public void comprar(int quantidade) {
-        nbitcoins+=quantidade ;
-        dinheiro -= corretora.venderBitcoin(quantidade);
-    }
+  
 
-    public void vender(int quantidade) {
-        nbitcoins-=quantidade;
-        dinheiro += corretora.comprarBitcoin(quantidade);
-    }
-
-    public void negociar() {
-        quantidade = gerador.nextInt(4) + 1;
-        if (nbitcoins == 0 && dinheiro >= corretora.valorBTC * quantidade && corretora.numBitcoins>=quantidade) {
-            this.comprar(quantidade);
-        } else if (gerador.nextInt(3) == 1 && dinheiro >= (corretora.valorBTC * quantidade) && corretora.numBitcoins>=quantidade) {
-            this.comprar(quantidade);
-        } else if (nbitcoins>=quantidade && corretora.dinheiro >= corretora.valorBTC * quantidade) {
-            this.vender(quantidade);
-        }
-
-    }
 
 }
--- src/bitcoin/Corretora.java
+++ src/bitcoin/Corretora.java
@@ -3,25 +3,30 @@ package bitcoin;
 import java.util.Random;
 
 public class Corretora {
-
+    
+    public Double valorBTC;
     public Double dinheiro;
     public Integer numBitcoins;
-    public Double valorBTC;
+  
     public Random gerador = new Random();
 
     public Corretora() {
+        this.valorBTC = 30305.00;
         this.dinheiro = 0.00;
         this.numBitcoins = 100;
-        this.valorBTC = 35780.00;
+     
     }
 
     public synchronized Double comprarBitcoin(int quantidade) {
-        numBitcoins+= quantidade;
-        dinheiro -= valorBTC * quantidade;
+        numBitcoins = numBitcoins + quantidade;
+        dinheiro = dinheiro - (valorBTC * quantidade);
         Double aux = valorBTC * quantidade;
+        
         int auxValor = gerador.nextInt(10001);
+        
         if(valorBTC - auxValor >=0){
         valorBTC-= auxValor;
+        
         }
         return aux;
     }
@@ -29,16 +34,19 @@ public class Corretora {
     public synchronized Double venderBitcoin(int quantidade) {
 
         while (numBitcoins == 0) {
-            try {
+            try{
                 wait(3000);
-            } catch (InterruptedException ex) {
+               
+            } 
+            catch (InterruptedException ex) {
+                
             }
         }
        
-            numBitcoins-= quantidade;
-            dinheiro += valorBTC * quantidade;
+            numBitcoins = numBitcoins - quantidade;
+            dinheiro = dinheiro + (valorBTC * quantidade);
             Double aux = valorBTC * quantidade;
-            valorBTC += gerador.nextInt(10001);
+            valorBTC = valorBTC + (gerador.nextInt(10001));
             notify();
             return aux;
 }
